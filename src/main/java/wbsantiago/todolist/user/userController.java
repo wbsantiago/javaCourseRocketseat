d792af6c7a6fa336e81e2d2cfc537d3e28adbf64
wbsantiago <will.jimmy27@hotmail.com>
2023-10-11 20:03:31 -0300

    end of second class

--- src/main/java/wbsantiago/todolist/user/userController.java
+++ src/main/java/wbsantiago/todolist/user/userController.java
@@ -1,6 +1,8 @@
 package wbsantiago.todolist.user;
 
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,16 +26,18 @@ public class userController {
     */
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null) {
             System.out.println("Usuario ja existe");
-            return null;
+            //mensagem de erro
+            //status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
